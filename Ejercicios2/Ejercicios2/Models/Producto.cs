6c6b1b6369d99d079190a7432b7273188bd7f6bd
Luis RIos <blackwarrior_l@hotmail.com>
2017-09-22 20:14:02 -0500

    Modelo agregado

---
+++ Ejercicios2/Ejercicios2/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios2.Models
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string Precio { get; set; }
+        public string Tipo { get; set; }
+    }
+}
