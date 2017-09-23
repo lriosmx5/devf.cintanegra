77baf887cdc6f53e711d621942e10f35bf078db4
lriosmx5 <32182656+lriosmx5@users.noreply.github.com>
2017-09-22 21:03:39 -0500

    Al vuelo

--- Ejercicios2/Ejercicios2/Models/Producto.cs
+++ Ejercicios2/Ejercicios2/Models/Producto.cs
@@ -7,7 +7,16 @@ using System.Threading.Tasks;
 namespace Ejercicios2.Models
 {
     public class Producto
-    {
+    {   
+        public Producto() {}
+        
+        public Producto(string nombre, double precio, string tipo)
+        {
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Tipo = tipo;
+        }
+        
         public string Nombre { get; set; }
         public double Precio { get; set; }
         public string Tipo { get; set; }
